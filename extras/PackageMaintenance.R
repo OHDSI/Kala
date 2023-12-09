686d55c7bad29f2ad9f6d14ac6395c77cb3a4db5
Gowtham Rao <rao@ohdsi.org>
2023-12-09 07:27:38 -0500

    Clean up

--- extras/PackageMaintenance.R
+++ extras/PackageMaintenance.R
@@ -1,37 +1,44 @@
-# @file PackageMaintenance
-#
-# Copyright 2020 Observational Health Data Sciences and Informatics
+# Copyright 2023 Observational Health Data Sciences and Informatics
 #
 # This file is part of Kala
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Format and check codeP
-OhdsiRTools::formatRFolder()
+# Format and check code ---------------------------------------------------
+styler::style_pkg()
 OhdsiRTools::checkUsagePackage("Kala")
 OhdsiRTools::updateCopyrightYearFolder()
+
+# Devtools check -----------------------------------------------------------
 devtools::spell_check()
-spelling::spell_check_files(list.files(path = "inst/shiny", pattern = "*.html", recursive = TRUE, full.names = TRUE))
+devtools::check()
 
-
-# Create manual and vignettes:
+# Create manual -----------------------------------------------------------
 unlink("extras/Kala.pdf")
 shell("R CMD Rd2pdf ./ --output=extras/Kala.pdf")
 
-rmarkdown::render("vignettes/SimpleTimeSeriesAnalysisUsingKala.Rmd",
-                  output_file = "../inst/doc/SimpleTimeSeriesAnalysisUsingKala.pdf",
-                  rmarkdown::pdf_document(latex_engine = "pdflatex",
-                                          toc = TRUE,
-                                          number_sections = TRUE))
-
+# Create Vignettes---------------------------------------------------------
+dir.create(file.path("inst","doc"), showWarnings = FALSE)
+# Build site---------------------------------------------------------
 pkgdown::build_site()
+OhdsiRTools::fixHadesLogo()
+
+
+
+# Release package to CRAN ------------------------------------------------------
+# devtools::check_win_devel()
+# devtools::check_rhub()
+# devtools::release()
+# devtools::check(cran=TRUE)
+
+
