e4f9faf38151ce0c68a5edee93b1bf658b9076dc
ZorzArg <george.argyriou@odysseusinc.com>
2025-03-24 18:07:04 +0000

    added helper functions;

--- R/getFeatureExtractionReportByTimeWindows.R
+++ R/getFeatureExtractionReportByTimeWindows.R
@@ -1220,3 +1220,52 @@ getFeatureExtractionStandardizedDifferenceInParallel <-
     
     return(output)
   }
+
+
+commaSeparaedStringToIntArray <- function(inputString) {
+  # Split the string into elements based on commas
+  stringElements <- strsplit(inputString, ",")[[1]]
+  # Remove empty elements
+  stringElements <- stringElements[stringElements != ""]
+  # Convert elements to integer
+  integerArray <- as.double(stringElements)
+  return(integerArray)
+}
+
+
+formatCountPercent <- function (count, percent, percentDigits = 1) {
+  
+  return(paste0(formatIntegerWithComma(count), " (", formatPercent(percent, 
+                                                                   digits = percentDigits), ")"))
+}
+
+
+formatIntegerWithComma <- function (number) {
+  
+  return(formatC(number, format = "d", big.mark = ","))
+}
+
+formatDecimalWithComma <- function (number, decimalPlaces = 1, round = TRUE) {
+  
+  integerPart <- floor(number)
+  decimalPart <- number - integerPart
+  if (round) {
+    decimalPart <- round(decimalPart, decimalPlaces)
+  }
+  else {
+    decimalPart <- trunc(decimalPart * 10^decimalPlaces)/10^decimalPlaces
+  }
+  formattedIntegerPart <- formatC(integerPart, format = "d", 
+                                  big.mark = ",")
+  decimalPartAsString <- formatC(decimalPart, format = "f", 
+                                 digits = decimalPlaces)
+  formattedDecimalPart <- substr(decimalPartAsString, 3, nchar(decimalPartAsString))
+  return(paste(formattedIntegerPart, formattedDecimalPart, 
+               sep = "."))
+}
+
+formatPercent <- function (x, digits = 2, format = "f", ...) {
+  
+  paste0(formatC(100 * x, format = format, digits = digits, 
+                 ...), "%")
+}
