39e31d73af43e52b9bd96038c74359f0fbc615c5
Gowtham Rao <rao@ohdsi.org>
2025-03-24 06:32:54 -0400

    Package initiation

--- R/Kala.R
+++ R/Kala.R
@@ -1,13 +1,13 @@
-# Copyright 2020 Observational Health Data Sciences and Informatics
+# Copyright 2025 Observational Health Data Sciences and Informatics
 #
 # This file is part of Kala
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,5 +18,7 @@
 "_PACKAGE"
 
 #' @import DatabaseConnector
-#' @import magrittr
+#' @import dplyr
+#' @importFrom rlang .data
+#' @importFrom lifecycle deprecated
 NULL
